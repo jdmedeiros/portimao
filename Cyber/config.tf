cd6ef52db998241aab39be7e33b1b4f37d201c3a
Dan Medeiros <dan.medeiros@sagewell.com>
2023-03-15 11:58:28 -0100

    Terraform Lisboa e Ponta Delgada vFinal_rev2

--- Cyber/config.tf
+++ Cyber/config.tf
@@ -1,12 +1,20 @@
 data "template_file" "desktop" {
   template = <<EOF
 #!/bin/bash
+LOGFILE="/var/log/cloud-config-"$(date +%s)
+SCRIPT_LOG_DETAIL="${LOGFILE}"_$(basename "$0").log
+
+# Reference: https://serverfault.com/questions/103501/how-can-i-fully-log-all-bash-scripts-actions
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>$SCRIPT_LOG_DETAIL 2>&1
+
 hostnamectl set-hostname desktop
 apt-get update
 apt-get upgrade
 apt install -y xfce4 xfce4-goodies
 apt install -y xrdp filezilla
-install mysql-workbench-community
+apt install -y mysql-workbench-community
 snap connect mysql-workbench-community:password-manager-service :password-manager-service
 snap install brave
 snap install thunderbird
@@ -14,7 +22,6 @@ adduser xrdp ssl-cert
 echo -e "ubuntu\nPassw0rd" | passwd ubuntu
 echo xfce4-session > /home/ubuntu/.xsession
 chown ubuntu:ubuntu /home/ubuntu/.xsession
-sudo systemctl enable --now xrdp
-
+systemctl enable --now xrdp
 EOF
 }
