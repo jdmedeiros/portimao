ff374555a115cf1d32d6ad7866aa864997b7d5b3
Dan Medeiros <dan.medeiros@sagewell.com>
2023-03-16 22:53:52 -0100

    Show EIP

--- Cyber/config.tf
+++ Cyber/config.tf
@@ -43,9 +43,26 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>$SCRIPT_LOG_DETAIL 2>&1
 
+cat <<'EOF' > /tmp/50-cloud-init.yaml.patch
+--- 50-cloud-init.yaml_bak      2023-03-16 22:45:13.413033827 +0000
++++ 50-cloud-init.yaml  2023-03-16 22:44:13.691770064 +0000
+@@ -7,6 +7,10 @@
+     ethernets:
+         ens5:
+             dhcp4: true
++            routes:
++              - to: 0.0.0.0/0
++                via: 10.0.1.10
++                on-link: true
+             dhcp4-overrides:
+                 route-metric: 100
+             dhcp6: false
+EOF
+
+patch /etc/netplan/50-cloud-init.yaml < /tmp/50-cloud-init.yaml.patch
+netplan apply
 wget https://raw.githubusercontent.com/jdmedeiros/portimao/main/Cyber/50-cloud-init.yaml.patch
-patch /etc/netplan/50-cloud-init.yaml 50-cloud-init.yaml.patch
-patch /etc/netplan/50-cloud-init.yaml.patch < 50-cloud-init.yaml.patch
+
 hostnamectl set-hostname onion
 apt update && apt -y update && apt -y install git build-essential
 apt install -y xfce4 xfce4-goodies
